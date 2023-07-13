053f96f9c2432288b5e450466c847c5e088dade4
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-13 09:02:42 -0400

    patch and deleted updated

--- Recipe-Managment-API/server.js
+++ Recipe-Managment-API/server.js
@@ -2,17 +2,8 @@ const { RecipeInventory } = require('./models');
 const express = require('express');
 const app = express();
 const port = 4000;
-// const { query } = require('./database');
 require("dotenv").config();
 
-// app.use((req, res, next) => {
-//     console.log(`Request: ${req.method} ${req.originalUrl}`);
-//     res.on("finish", () => {
-//       // the 'finish' event will be emitted when the response is handed over to the OS
-//       console.log(`Response Status: ${res.statusCode}`);
-//     });
-//     next();
-//   });
 
 app.use(express.json());
 
@@ -35,6 +26,46 @@ app.get('/RecipeInventory', async (req, res) => {
         res.status(500).send( {message: error.message});
     }
 })
+
+app.patch('/RecipeInventory/:id', async (req, res) => {
+    const recipeId = parseInt(req.params.id, 10);
+  
+    try {
+      const [numberOfAffectedRows, affectedRows] = await RecipeInventory.update(
+        req.body,
+        { where: { id: recipeId }, returning: true }
+      );
+  
+      if (numberOfAffectedRows > 0) {
+        res.status(200).json(affectedRows[0]);
+      } else {
+        res.status(404).send({ message: 'Recipe not found' });
+      }
+    } catch (err) {
+      if (err.name === 'SequelizeValidationError') {
+        return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+      }
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+  app.delete('/RecipeInventory/:id', async (req, res) => {
+    const recipeId = parseInt(req.params.id, 10);
+  
+    try {
+      const deleteOp = await RecipeInventory.destroy({ where: { id: recipeId } });
+  
+      if (deleteOp > 0) {
+        res.status(200).send({ message: 'Recipe deleted successfully' });
+      } else {
+        res.status(404).send({ message: 'Recipe not found' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
